cc293df3690e83de33b9bfbf748205180a52e720
Sam Cohen <sbcohen2000@gmail.com>
2021-09-18 13:25:28 -0400

    Rewrote the graph structure
    The new implementation does not keep track of the nodes themselves,
    only the connections between them (it only implements the adjacency
    matrix). Now, edges may contain arbitrary data, not just booleans.

--- lib_core/Graph.ml
+++ lib_core/Graph.ml
@@ -1,58 +1,62 @@
-type 'a t = 'a array * bool array array
+exception Out_of_bounds of string
+type 'a t = 'a option array array option
 
-let create (node : 'a) =
-  [| node |], [| [| false |] |]
-  
-let print (graph : 'a t) =
-  let _, connections = graph in
-  Array.iteri (fun i row ->
-      let strs = Array.map (fun b -> if b then "t" else "_") row in
-      print_endline (Int.to_string i ^ ": " ^ String.concat " " (Array.to_list strs))
-    ) connections
+let empty = None
 
-let add_node (graph : 'a t) (node : 'a) =
-  let nodes, connections = graph in
-  let nodes' = Array.append nodes [| node |] in
-  let new_node_idx = Array.length nodes - 1 in
-  let connections' = Array.map (fun row ->
-                         Array.append row [| false |]) connections in
-  let connections' = Array.append connections'
-                       [| Array.make (new_node_idx + 2) false |] in
-  nodes', connections'
+let create (n : int) =
+  Some (Array.make n (Array.make n None))
 
-let update_node (graph : 'a t) (idx : int) (node : 'a) =
-  let nodes, _ = graph in
-  Array.set nodes idx node
+let size (g : 'a t) =
+  match g with
+  | Some m -> Array.length m
+  | None -> 0
 
-let nodes (graph : 'a t) =
-  let nodes, _ = graph in nodes
+let edge (g : 'a t) (orig : int) (dest : int) =
+  match g with
+  | None -> None
+  | Some m -> Array.get (Array.get m orig) dest
 
-let size (graph : 'a t) =
-  let nodes, _ = graph in Array.length nodes
+let edges (g : 'a t) (orig : int) =
+  match g with
+  | None -> []
+  | Some m ->
+     let row = Array.get m orig in
+     Array.fold_left (fun lst edge ->
+         match edge with
+         | Some e -> e::lst
+         | None -> lst) [] row
 
-let connect (graph : 'a t) (a_idx : int) (b_idx : int) =
-  let _, connections = graph in
-  Array.set (Array.get connections a_idx) b_idx true
+let neighbors (g : 'a t) (orig : int) =
+  match g with
+  | None -> []
+  | Some m ->
+     let row = Array.get m orig in
+     let _, lst = 
+       Array.fold_left (fun (idx, lst) edge ->
+           match edge with
+           | Some _ -> (idx + 1), idx::lst
+           | None -> (idx + 1), lst) (0, []) row in
+     lst
 
-let disconnect (graph : 'a t) (a_idx : int) (b_idx : int) = 
-  let _, connections = graph in
-  Array.set (Array.get connections a_idx) b_idx false
+(* copies 'from' into the upper
+ * right matrix of 'into' *)
+let copy (from : 'a t) (into : 'a t) =
+  match (from, into) with
+  | (Some f_mat, Some to_mat) ->
+     Array.iteri (fun rn col ->
+         Array.iteri (fun cn edge ->
+             Array.set (Array.get to_mat rn) cn edge)
+           col) f_mat
+  | _ -> ()
 
-let neighbor_indices (graph : 'a t) (idx : int) =
-  let _, connections = graph in
-  let row = Array.get connections idx in
-  let _, lst = Array.fold_left (fun (idx, lst) b ->
-                   if b then (idx + 1, idx::lst)
-                   else (idx + 1, lst)) (0, []) row in
-  lst
+(* increase the capacity of the
+ * graph by 1 *)
+let expand (g : 'a t) =
+  let new_g = create (size g + 1) in
+  copy g new_g;
+  new_g
 
-let at (graph : 'a t) (idx : int) =
-  let nodes, _ = graph in
-  Array.get nodes idx
-
-let neighbors (graph : 'a t) (idx : int) =
-  let indices = neighbor_indices graph idx in
-  let f = at graph in
-  List.map f indices
-
-
+let connect (g : 'a t) (orig : int) (dest : int) (edge : 'a) =
+  match g with
+  | None -> raise (Out_of_bounds "connect: graph is empty")
+  | Some m -> Array.set (Array.get m orig) dest (Some edge)
